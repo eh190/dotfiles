f63a04949a7a078a4a17609816c153625cb58863
Ed Hughes <edward@fxdigital.uk>
2022-11-10 21:16:35 +0000

    add new colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,7 @@
 vim.o.background = "dark" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
-vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd([[colorscheme tokyonight-night]])
 -- vim.cmd [[colorscheme nord]]
+
+-- lunarvim/darkplus theme
+-- vim.cmd("colorscheme darkplus")

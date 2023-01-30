6fdcf3c27e88df3b698e7902fa9a53a8764f5bca
Ed Hughes <edward@fxdigital.uk>
2023-01-30 16:45:54 +0000

    add sorcery and doom one colour schemes

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
 vim.o.background = "dark" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[colorscheme tokyonight-night]])
--- vim.cmd [[colorscheme nord]]
+-- vim.cmd([[colorscheme srcery]])
+-- vim.cmd([[colorscheme doom-one]])
 
 -- lunarvim/darkplus theme
 -- vim.cmd("colorscheme darkplus")
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -49,9 +49,10 @@ return packer.startup(function(use)
 	-- Colorschemes
 	-- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
 	use({ "ellisonleao/gruvbox.nvim" })
-	use("shaunsingh/nord.nvim")
 	use("folke/tokyonight.nvim")
 	use("lunarvim/darkplus.nvim")
+	use("NTBBloodbath/doom-one.nvim")
+	use({ "srcery-colors/srcery-vim", as = "srcery" })
 
 	-- cmp plugins
 	use("hrsh7th/nvim-cmp") -- The completion plugin

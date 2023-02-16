51652f4fe7ca6d339cf2d7bb8ae4c12e64a93777
Ed Hughes <edward@fxdigital.uk>
2023-02-16 16:36:04 +0000

    fix lsp document_highlight on words

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,12 @@
 vim.o.background = "dark" -- or "light" for light mode
--- vim.cmd([[colorscheme gruvbox]])
--- vim.cmd([[colorscheme tokyonight-night]])
--- vim.cmd([[colorscheme srcery]])
-vim.cmd([[colorscheme doom-one]])
 
+-- vim.cmd([[colorscheme srcery]])
+
+-- Colorschemes that support the :lua vim.lsp.buf.document_highlight() (handlers.lua)
 -- lunarvim/darkplus theme
--- vim.cmd("colorscheme darkplus")
+vim.cmd("colorscheme darkplus")
+-- vim.cmd([[colorscheme tokyonight-night]])
+
+-- These two support it but not with a highlight block behind the word
+-- vim.cmd([[colorscheme doom-one]])
+-- vim.cmd([[colorscheme gruvbox]])

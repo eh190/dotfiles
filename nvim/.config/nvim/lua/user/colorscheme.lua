284f189bf1494e338bf230f243eb6cf4c95e9ac9
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2023-04-20 18:05:08 +0100

    update colourscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -4,10 +4,15 @@ vim.o.background = "dark" -- or "light" for light mode
 
 -- Colorschemes that support the :lua vim.lsp.buf.document_highlight() (handlers.lua)
 -- vim.cmd("colorscheme darkplus")
+
 -- vim.cmd([[colorscheme tokyonight-night]])
-vim.cmd([[colorscheme kanagawa]])
+-- vim.cmd([[colorscheme tokyonight-day]])
+
+vim.cmd("colorscheme kanagawa")
+-- dark dark
+-- vim.cmd("colorscheme kanagawa-dragon")
+-- light
 -- vim.cmd("colorscheme kanagawa-lotus")
--- vim.cmd("colorscheme kanagawa-wave")
 
 -- These two support it but not with a highlight block behind the word
 -- vim.cmd([[colorscheme doom-one]])

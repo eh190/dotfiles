7b1d9f415ee2af96f9bbe12642ce88da74b2d561
Ed Hughes <edward@fxdigital.uk>
2023-05-18 10:59:24 +0100

    make filepaths absolute in telescope

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -90,4 +90,17 @@ telescope.setup({
 			},
 		},
 	},
+	pickers = {
+		-- Default configuration for builtin pickers goes here:
+		-- picker_name = {
+		--   picker_config_key = value,
+		--   ...
+		-- }
+		-- Now the picker_config_key will be applied every time you call this
+		-- builtin picker
+		find_files = {
+			-- return absolute paths
+			path_display = { "absolute" },
+		},
+	},
 })

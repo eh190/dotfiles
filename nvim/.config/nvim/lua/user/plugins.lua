55fec4b4c933fd7eb972fdafd453faa89a508f99
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2023-07-15 16:48:37 +0100

    remove lualine

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -100,12 +100,6 @@ return packer.startup(function(use)
 	use("kyazdani42/nvim-web-devicons")
 	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
 
-	-- Lualine
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-
 	-- Gitsigns
 	use("lewis6991/gitsigns.nvim")
 

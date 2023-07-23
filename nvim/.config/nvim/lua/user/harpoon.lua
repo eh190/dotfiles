81d5a47cb910ac5cb6d9f7411c3cd0f4f064da02
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2023-07-23 21:51:13 +0100

    add harpoon

---
+++ nvim/.config/nvim/lua/user/harpoon.lua
@@ -0,0 +1,22 @@
+local mark_status_ok, mark = pcall(require, "harpoon.mark")
+if not mark_status_ok then
+	return
+end
+
+local ui_status_ok, ui = pcall(require, "harpoon.ui")
+if not ui_status_ok then
+	return
+end
+
+vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-a>", function()
+	ui.nav_file(1)
+end)
+vim.keymap.set("n", "<C-b>", function()
+	ui.nav_file(2)
+end)
+vim.keymap.set("n", "<C-c>", function()
+	ui.nav_file(3)
+end)
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -109,6 +109,9 @@ return packer.startup(function(use)
 	-- Save Sessions
 	use("Shatur/neovim-session-manager")
 
+	-- Harpoon
+	use("ThePrimeagen/harpoon")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

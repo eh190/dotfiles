e8e90725bd82d3f84bb90c4791de2f033a8cc306
Ed Hughes <ed.hughes@sparkbox.ai>
2023-07-24 11:34:34 +0100

    add bufferline back, update harpoon width

--- nvim/.config/nvim/lua/user/harpoon.lua
+++ nvim/.config/nvim/lua/user/harpoon.lua
@@ -1,3 +1,8 @@
+local harpoon_status, harpoon = pcall(require, "harpoon")
+if not harpoon_status then
+	return
+end
+
 local mark_status_ok, mark = pcall(require, "harpoon.mark")
 if not mark_status_ok then
 	return
@@ -7,6 +12,12 @@ local ui_status_ok, ui = pcall(require, "harpoon.ui")
 if not ui_status_ok then
 	return
 end
+
+harpoon.setup({
+	menu = {
+		width = vim.api.nvim_win_get_width(0) - 60,
+	},
+})
 
 vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu)

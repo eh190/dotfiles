082537114760d51d1ef28d48422e419c6983235e
Ed Hughes <ed.hughes@sparkbox.ai>
2023-11-06 15:46:33 +0000

    nvim: colourscheme change git higlighting

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -5,4 +5,9 @@ vim.o.background = "dark" -- or "light" for light mode
 -- vim.cmd("colorscheme kanagawa")
 -- vim.cmd("colorscheme kanagawa-lotus") -- light
 
-vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme rose-pine');
+
+vim.cmd('hi DiffAdd blend=0 guifg=#252525 guibg=#587c0c')
+vim.cmd('hi DiffChange blend=0 cterm=underline gui=underline guifg=#252525 guibg=#0c7d9d')
+vim.cmd('hi DiffDelete blend=0 guifg=#252525 guibg=#94151b')
+vim.cmd('hi DiffText blend=0 guifg=#252525 guibg=#94151b')

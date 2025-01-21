af25453c0c82fee37f28afc6cdfa520c7f39ee8f
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2025-01-21 10:50:24 +0000

    colourscheme comments

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -37,6 +37,41 @@ if not status_ok then
 	return
 end
 
+local colors_status_ok, colors = pcall(require, "vscode.colors")
+if not colors_status_ok then
+	return
+end
+
 vscode.setup({})
+-- local c = colors.get_colors()
+-- vscode.setup({
+--    -- Alternatively set style in setup
+--     -- style = 'light'
+
+--     -- Enable transparent background
+    -- transparent = true,
+
+--     -- Enable italic comment
+--     italic_comments = true,
+
+--     -- Underline `@markup.link.*` variants
+--     underline_links = true,
+
+--     -- Disable nvim-tree background color
+--     disable_nvimtree_bg = true,
+
+--     -- Override colors (see ./lua/vscode/colors.lua)
+    -- color_overrides = {
+    --     vscLineNumber = '#FFFFFF',
+    -- },
+
+--     -- Override highlight groups (see ./lua/vscode/theme.lua)
+    -- group_overrides = {
+--         -- this supports the same val table as vim.api.nvim_set_hl
+--         -- use colors from this colorscheme by requiring vscode.colors!
+    --     Cursor = { fg = c.vscCursorLight, bg = c.vscCursorLight},
+    -- CursorLine ={ fg = c.vscCursorLight, bg = c.vscCursorLight} 
+    -- }
+-- })
 
 vim.cmd('colorscheme vscode');
